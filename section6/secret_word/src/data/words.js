a85039cc52f6c9ed05ff6238fa2825412762d226
LucasTMB <lucas.tmb708@gmail.com>
2022-12-18 11:36:47 -0300

    Iniciando o projeto Secret Worde estilizando o start

---
+++ section6/secret_word/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ['Motor', 'Porta', 'Capô', 'Pneu', 'Antena'],
+    fruta: ['Banana', 'Maçã', 'Pêra', 'Mamão', 'Laranja'],
+    corpo: ['Braço', 'Perna', 'Cérebro', 'Pescoço', 'Olhos'],
+    computador: ['Mouse', 'Teclado', 'Monitor', 'Gabinete', 'Webcam'],
+    programação: ['Linguagem', 'Framework', 'React', 'JavaScript', 'Python'],
+    alimento: ['Arroz', 'Feijão', 'Carne', 'Leite', 'Ovo'],
+};
