aa689509ebbb8181c71902d24548139cc196498e
LucasTMB <lucas.tmb708@gmail.com>
2022-12-20 20:33:38 -0300

    Escolhendo categorias e palavras de forma aleatória

--- section6/secret_word/src/App.js
+++ section6/secret_word/src/App.js
@@ -19,8 +19,41 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCategory, setPickedCategory] = useState('');
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    // pegando uma categoria aleatória
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    console.log(category)
+
+    // pegando uma palavra aleatória
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    console.log(word)
+
+    return {word, category}
+  }
+
   // iniciando o jogo
   const startGame = () => {
+    // capturando uma palavra e uma categoria
+    const {word, category} = pickWordAndCategory();
+    console.log(word, category);
+
+    // criando um array de letras
+    let wordLetters = word.split('');
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+    console.log(wordLetters);
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters)
+
     setGameStage(stages[1].name)
   }
 
