e5b94b011055b598a4b014d7b507e02820c831ba
LucasTMB <lucas.tmb708@gmail.com>
2022-12-21 22:21:52 -0300

    Estruturando o componente do jogo e estilizando o CSS dele

--- section6/secret_word/src/components/GameScreen.js
+++ section6/secret_word/src/components/GameScreen.js
@@ -2,9 +2,31 @@ import './GameScreen.css';
 
 const GameScreen = ({verifyLetter}) => {
   return (
-    <div>
-        <h2>Game Screen</h2>
-        <button onClick={verifyLetter}>Verificar letra</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <p>Você ainda tem XXX tentativas</p>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength='1' required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLetterContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   )
 }
