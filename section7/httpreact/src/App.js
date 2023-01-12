fbc6f05180b4bfc09548aa677baf58f677971536
LucasTMB <lucas.tmb708@gmail.com>
2023-01-11 21:29:48 -0300

    Adicionando dados utilizando o método POST

--- section7/httpreact/src/App.js
+++ section7/httpreact/src/App.js
@@ -8,6 +8,9 @@ function App() {
 
   const [products, setProducts] = useState([]);
 
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+
   // 1 - Resgatando dados
   useEffect(() => {
 
@@ -21,7 +24,25 @@ function App() {
     fetchData();
   }, []);
 
-  console.log(products);
+  //2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+    });
+
+    
+  }
 
   return (
     <div className="App">
@@ -31,6 +52,20 @@ function App() {
           <li key={product.id}>{product.name} - R$ {product.price}</li>
         ))}
       </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)}/>
+          </label>
+          <label>
+            Preço:
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 }
