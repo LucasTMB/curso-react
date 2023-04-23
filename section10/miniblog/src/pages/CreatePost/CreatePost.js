65f7b5b1876545909f9f4d56211220fe2263c792
LucasTMB <lucas.tmb708@gmail.com>
2023-04-22 22:47:38 -0300

    Finalizando a tela de criar post

--- section10/miniblog/src/pages/CreatePost/CreatePost.js
+++ section10/miniblog/src/pages/CreatePost/CreatePost.js
@@ -3,6 +3,7 @@ import styles from "./CreatePost.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
@@ -11,8 +12,44 @@ const CreatePost = () => {
   const [tags, setTags] = useState([]);
   const [formError, setFormError] = useState("");
 
+  const {user} = useAuthValue();
+
+  const {insertDocument, response} = useInsertDocument("posts");
+
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError("");
+
+    // validate image url
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.")
+    }
+
+    // criar array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    // checar todos os valores
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    if (formError) return;
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createBy: user.displayName
+    })
+
+    // redirect to home page
+    navigate("/");
   }
 
   return (
@@ -59,13 +96,14 @@ const CreatePost = () => {
             name="tags" 
             required 
             placeholder="Insira as tags separadas por vírgula"
-            onChange={(e) => setImage(e.target.value)}
+            onChange={(e) => setTags(e.target.value)}
             value={tags} 
           />
         </label>
-        {!loading && <button className="btn">Cadastrar</button>}
-        {loading && <button className="btn" disabled>Aguarde...</button>}
-        {error && <p className="error">{error}</p>}
+        {!response.loading && <button className="btn">Cadastrar</button>}
+        {response.loading && <button className="btn" disabled>Aguarde...</button>}
+        {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   )
